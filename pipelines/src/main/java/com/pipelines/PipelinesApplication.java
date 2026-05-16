5a8ef5256d4f1cc50ca63f2c8a285bf35bd6a60b
Kreucyu <elciocleiton72@gmail.com>
2026-05-16 19:05:09 -0300

    fix: teste de configuração

--- pipelines/src/main/java/com/pipelines/PipelinesApplication.java
+++ pipelines/src/main/java/com/pipelines/PipelinesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PipelinesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("testando");
 		SpringApplication.run(PipelinesApplication.class, args);
 	}
 
